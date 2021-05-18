64ce944e25b7b8527654014f9955c6f2847eac6e
Ivan Solomichev <vanijatko.social@gmail.com>
2021-05-18 16:36:17 +0300

    in progress

--- contracts/NV_DAO.sol
+++ contracts/NV_DAO.sol
@@ -16,28 +16,37 @@ contract NV_DAO is Ownable {
     PortfolioRiskType riskType;
     string name;
     address[] tokens;
-    mapping(address => bool) tokenUsed;
+    mapping(address => uint256) tokenIndex;
   }
 
-  address public escrowUSDT;
+  address public escrowLow;
+  address public escrowMid;
+  address public escrowHigh;
+
   uint256 public minInvestmentUSDT;
-  Portfolio[] private portfolios;
+  mapping(PortfolioRiskType => Portfolio) public portfolios;
   
   
-  modifier onlyExistingPortfolio(uint8 _id) {
-      require(_id < portfolios.length, "Wrong portfolio id");
-      _;
+  modifier onlyExistingPortfolio(uint8 _riskType) {
+    require(portfolios[PortfolioRiskType(_riskType)].tokens.length > 0, "Wrong portfolio _riskType");
+    _;
   }
 
 
   /**
    * @dev Constructs Smart Contract.
-   * @param _escrowUSDT Escrow used for USDT storage.
+   * @param _escrowLow Escrow low.
+   * @param _escrowMid Escrow mid.
+   * @param _escrowHigh Escrow high.
    */
-  constructor(address _escrowUSDT) {
-    require(_escrowUSDT != address(0), "Wrong escrowUSDT");
+  constructor(address _escrowLow, address _escrowMid, address _escrowHigh) {
+    require(_escrowLow != address(0), "Wrong _escrowLow");
+    require(_escrowMid != address(0), "Wrong _escrowMid");
+    require(_escrowHigh != address(0), "Wrong _escrowHigh");
 
-    escrowUSDT = _escrowUSDT;
+    _escrowLow = _escrowLow;
+    _escrowMid = _escrowMid;
+    _escrowHigh = _escrowHigh;
   }
 
   /**
@@ -50,51 +59,103 @@ contract NV_DAO is Ownable {
   }
 
   /**
-   * @dev Creates portfolio and adde to portfolios array.
-   * @param _riskType Portfolio risk tolerance.
+   * @dev Creates portfolio and adds to portfolios array.
+   * @param _riskType Portfolio risk tolerance. 0 - low, 1 - mid, 2 - high
    * @param _name Portfolio name.
    * @param _tokens Tokens used for Portfolio trading.
    */
   function addPortfolio(uint8 _riskType, string memory _name, address[] memory _tokens) external onlyOwner {
     require(PortfolioRiskType(_riskType) <= PortfolioRiskType.high, "Wrong risk");
     require(bytes(_name).length > 0, "Empty name");
+    require(bytes(portfolios[PortfolioRiskType(_riskType)].name).length == 0, "Risk type present");
 
-    for(uint8 i = 0; i < portfolios.length; i ++) {
-      require(portfolios[i].riskType != PortfolioRiskType(_riskType), "Risk is present");
-      require(keccak256(bytes(portfolios[i].name)) != keccak256(bytes(_name)), "Name used");
-    }
-
-    uint256 id = portfolios.length;
-    portfolios[id].riskType = PortfolioRiskType(_riskType);
-    portfolios[id].name = _name;
-    portfolios[id].tokens = _tokens;
+    portfolios[PortfolioRiskType(_riskType)].riskType = PortfolioRiskType(_riskType);
+    portfolios[PortfolioRiskType(_riskType)].name = _name;
+    portfolios[PortfolioRiskType(_riskType)].tokens = _tokens;
     
-    for(uint8 i = 0; i < _tokens.length; i ++) {
+    for (uint8 i = 0; i < _tokens.length; i ++) {
       address tokenAddr = _tokens[i];
       require(tokenAddr != address(0), "Wrong token address");
-      portfolios[id].tokenUsed[tokenAddr] = true;
+      portfolios[PortfolioRiskType(_riskType)].tokenIndex[tokenAddr] = i;
+    }
+  }
+
+  /**
+   * @dev Adds tokens to existing portfolio.
+   * @param _riskType Portfolio risk tolerance to be updated.
+   * @param _tokens Tokens to be added to portfolio.
+   */
+  function addTokensToPortfolio(uint8 _riskType, address[] memory _tokens) external onlyOwner {
+    Portfolio storage portfolio = portfolios[PortfolioRiskType(_riskType)];
+    require(bytes(portfolio.name).length > 0, "No portfolio");
+
+    for (uint8 i = 0; i < _tokens.length; i ++) {
+      address tokenAddr = _tokens[i];
+      require(tokenAddr != address(0), "Wrong token address");
+      require(!isTokenAddedToPortfolio(tokenAddr, _riskType), "Token added");
+
+      portfolio.tokenIndex[tokenAddr] = portfolio.tokens.length;
+      portfolio.tokens.push(tokenAddr);
+    }
+  }
+
+  /**
+   * @dev Removes tokens to existing portfolio.
+   * @param _riskType Portfolio risk tolerance to be updated.
+   * @param _tokens Tokens to be removed from portfolio.
+   */
+  function removeTokensToPortfolio(uint8 _riskType, address[] memory _tokens) external onlyOwner {
+    Portfolio storage portfolio = portfolios[PortfolioRiskType(_riskType)];
+    require(bytes(portfolio.name).length > 0, "No portfolio");
+
+    for (uint8 i = 0; i < _tokens.length; i ++) {
+      address tokenAddr = _tokens[i];
+      require(isTokenAddedToPortfolio(tokenAddr, _riskType), "Token not added");
+      
+      uint256 idxToRemove = portfolio.tokenIndex[tokenAddr];
+      address lastAddress = portfolio.tokens[portfolio.tokens.length-1];
+
+      portfolio.tokenIndex[lastAddress] = idxToRemove;
+      portfolio.tokens.pop();
     }
   }
 
   /**
    * @dev Gets portfoilio details.
-   * @param _id Portfolio id.
-   * @return _riskType Portfolio risk type.
-   * @return _name Portfolio name.
-   * @return _tokens Tokens used for Portfolio trading.
+   * @param _riskType Portfolio risk type.
+   * @return riskType Portfolio risk type.
+   * @return name Portfolio name.
+   * @return tokens Tokens used for Portfolio trading.
    */
-  function portfolioInfo(uint8 _id) external view onlyExistingPortfolio(_id) returns (uint8 _riskType, string memory _name, address[] memory _tokens) {
-    _riskType = uint8(portfolios[_id].riskType);
-    _name = portfolios[_id].name;
-    _tokens = portfolios[_id].tokens;
+  function portfolioInfo(uint8 _riskType) external view onlyExistingPortfolio(_riskType) returns (uint8 riskType, string memory name, address[] memory tokens) {
+    riskType = uint8(portfolios[PortfolioRiskType(_riskType)].riskType);
+    name = portfolios[PortfolioRiskType(_riskType)].name;
+    tokens = portfolios[PortfolioRiskType(_riskType)].tokens;
+  }
+
+  /**
+   * @dev Checks if token is added to portfolio.
+   * @param _address Token address.
+   * @param _risk Portfolio risk.
+   */
+  function isTokenAddedToPortfolio(address _address, uint8 _risk) public view returns (bool) {
+    Portfolio storage portfolio = portfolios[PortfolioRiskType(_risk)];
+    if (portfolio.tokens.length == 0) {
+        return false;
+    }
+    
+    uint256 idx = portfolio.tokenIndex[_address];
+    return portfolio.tokens[idx] == _address;
   }
 
   /**
    * @dev Invests into portfolio.
-   * @param _id Portfolio id.
+   * @param _riskType Portfolio risk type.
    * @param _amount Investment amount in USDT.
    */
-  function invest(uint8 _id, uint256 _amount) external onlyExistingPortfolio(_id) {
+  function invest(uint8 _riskType, uint256 _amount) external onlyExistingPortfolio(_riskType) {
     require(_amount > minInvestmentUSDT, "Wrong _amount");
+
+    
   }
 }
