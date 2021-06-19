a004ea349c9e73291c0af5b0781b1844a85a92e6
Ivan Solomichev <vanijatko.social@gmail.com>
2021-06-19 21:14:32 +0300

    in progress

--- contracts/NV_DevFeeDistributionManager.sol
+++ contracts/NV_DevFeeDistributionManager.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
-pragma solidity >=0.4.22 <0.9.0;
+pragma solidity 0.8.5;
 
 contract NV_DevFeeDistributionManager {
-  constructor() public {
+  constructor() {
   }
 }
